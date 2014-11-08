26a62f3678ed83e8c2c805d8711bf40f5aed7197
hachepunto <hachepunto@gmail.com>
2014-11-08 11:31:56 -0600

    impruve scripts

--- AnimalTFDB_MARINA.R
+++ AnimalTFDB_MARINA.R
@@ -4,9 +4,13 @@
 
 ######## LOADS ########
 
+setwd(~/MARINa/)
+
 library(ssmarina)
 library(mixtools)
 load("/home/hachepunto/MARINa/general_MARINA.RData")
+to_clean <- ls()
+print("Source loaded")
 
 
 ########  R E G U L O N ######## 
@@ -14,55 +18,75 @@ load("/home/hachepunto/MARINa/general_MARINA.RData")
 
 # adj from ARACNe imput data
 TFDB_adjfile <- file.path("/home/hachepunto/MARINa/AnimalTFDB_p1e-10.adj")
+print("adj loaded")
 
+
+print("proced to calculet regulon")
 # Regulon generation
 
 TFDB_regulon <- aracne2regulon(TFDB_adjfile, dset)
 
 
+print("regulon calculated")
 ########  M A R I N A ######## 
-
+print("proced to calculet  master regulators")
 TFDB_mrs <- marina(signature, TFDB_regulon, nullmodel)
 
 
 # Leading-edge analysis
 TFDB_mrs <- ledge(TFDB_mrs)
 
+print("master regulators calculated")
 
+summary(TFDB_mrs)
 
+TFDB_top_100 <-summary(TFDB_mrs,mrs=100)
+write.table(TFDB_top_100,file = "TFDB_top_100mrs.txt",quote = FALSE, sep = "\t")
+
+print("Top 100 saved")
 ########  B O O T S T R A P E D   M A R I N A ######## 
-TFDB_mrs_BT <- marina(BTsignature, TFDB_regulon, nullmodel)
+print("proced to calculet  bootstraped master regulators")
+TFDB_BT_mrs <- marina(BTsignature, TFDB_regulon, nullmodel)
 
 
-TFDB_mrs_BT <- bootstrapMarina(mrs, "mode")
+TFDB_BT_mrs <- bootstrapMarina(TFDB_BT_mrs, "mode")
+print("master regulators bootstraped calculated")
+summary(TFDB_BT_mrs)
 
+TFDB_BT_top_100 <-summary(TFDB_BT_mrs,mrs=100)
+write.table(TFDB_BT_top_100,file = "TFDB_BT_top_100mrs.txt",quote = FALSE, sep = "\t")
 
+print("Top 100 bootstraped saved")
 
+print("proced to Shadow and Synergy analysis")
 #######  S H A D O W and S Y N E R G Y A N A L Y S I S #########
 
 # Shadow analysis
 TFDB_mrshadow <- shadow(TFDB_mrs, pval=25)
+# Bootstraped Shadow analysis
+TFDB_mrshadow_BT <- shadow(TFDB_BT_mrs, pval=25)
 
+print("Shadow analysis done")
+
+
+print("proced to Synergy analysis")
 
 # Synergy analysis
 TFDB_mrs <- marinaCombinatorial(TFDB_mrs, regulators=25) 
 
-
-# Bootstraped Shadow analysis
-TFDB_mrshadow_BT <- shadow(TFDB_mrs_BT, pval=25)
-
-
 # Bootstraped Synergy analysis
-TFDB_mrs_BT <- marinaCombinatorial(TFDB_mrs_BT, regulators=25) 
+TFDB_BT_mrs <- marinaCombinatorial(TFDB_BT_mrs, regulators=25) 
+print("Synergy analysis done")
 
+
+print("proced to Save")
 
 ###################################################################################
 
 
 ##################################################
+rm(list=to_clean,to_clean) #cleaning
+save.image("TFDB_MARINa.RData")
+savehistory("TFDB_MARINa.Rhistory")
 
-
-save.image("/home/hachepunto/MARINa/TFDB_MARINa.RData")
-savehistory("/home/hachepunto/MARINa/TFDB_MARINa.Rhistory")
-
-
+print("All done")
--- Vaquerizas_MARINA.R
+++ Vaquerizas_MARINA.R
@@ -4,10 +4,14 @@
 
 ######## LOADS ########
 
+setwd(~/MARINa/)
+
 library(ssmarina)
 library(mixtools)
-load("/home/hachepunto/MARINa/general_MARINA.RData")
+load("general_MARINA.RData")
+to_clean <- ls()
 
+print("Source loaded")
 
 ########  R E G U L O N ######## 
 ### One proces by TF list
@@ -15,10 +19,17 @@ load("/home/hachepunto/MARINa/general_MARINA.RData")
 # adj from ARACNe imput data
 Vaquerizas_adjfile <- file.path("/home/hachepunto/MARINa/Vaquerizas_p1e-10.adj")
 
-# Regulon generation
+print("adj loaded")
+
+
+print("proced to calculet regulon")
 
 Vaquerizas_regulon <- aracne2regulon(Vaquerizas_adjfile, dset)
 
+print("regulon calculated")
+
+
+print("proced to calculet  master regulators")
 
 ########  M A R I N A ######## 
 
@@ -27,34 +38,67 @@ Vaquerizas_mrs <- marina(signature, Vaquerizas_regulon, nullmodel)
 # Leading-edge analysis
 Vaquerizas_mrs <- ledge(Vaquerizas_mrs)
 
+print("master regulators calculated")
+summary(Vaquerizas_mrs)
+
+
+Vaquerizas_top_100 <-summary(Vaquerizas_mrs,mrs=100)
+write.table(Vaquerizas_top_100,file = "Vaquerizas_top_100mrs.txt",quote = FALSE, sep = "\t")
+
+print("Top 100 master regulators saved")
+
+
+print("proced to calculet  bootstraped master regulators")
 
 ########  B O O T S T R A P E D   M A R I N A ######## 
 
-Vaquerizas_mrs_BT <- marina(BTsignature, Vaquerizas_regulon, nullmodel)
+Vaquerizas_BT_mrs <- marina(BTsignature, Vaquerizas_regulon, nullmodel)
 
-Vaquerizas_mrs_BT <- bootstrapMarina(mrs, "mode")
+Vaquerizas_BT_mrs <- bootstrapMarina(Vaquerizas_BT_mrs , "mode")
 
+print("master regulators bootstraped calculated")
+summary(Vaquerizas_BT_mrs)
 
+TFDB_BT_top_100 <-summary(Vaquerizas_BT_mrs,mrs=100)
+write.table(TFDB_BT_top_100,file = "TFDB_BT_top_100mrs.txt",quote = FALSE, sep = "\t")
+
+print("Top 100 bootstraped saved")
+
+print("proced to Shadow and Synergy analysis")
+print("proced to Shadow analysis")
 #######  S H A D O W and S Y N E R G Y A N A L Y S I S #########
 
 # Shadow analysis
 Vaquerizas_mrshadow <- shadow(Vaquerizas_mrs, pval=25)
 
+# Bootstraped Shadow analysis
+Vaquerizas_mrshadow_BT <- shadow(Vaquerizas_BT_mrs, pval=25)
+
+print("Shadow analysis done")
+
+
+print("proced to Synergy analysis")
+
 # Synergy analysis
 Vaquerizas_mrs <- marinaCombinatorial(Vaquerizas_mrs, regulators=25)
 
-# Bootstraped Shadow analysis
-Vaquerizas_mrshadow_BT <- shadow(Vaquerizas_mrs_BT, pval=25)
 
 # Bootstraped Synergy analysis
-Vaquerizas_mrs_BT <- marinaCombinatorial(Vaquerizas_mrs_BT, regulators=25)
+Vaquerizas_BT_mrs <- marinaCombinatorial(Vaquerizas_BT_mrs, regulators=25)
 
+print("Synergy analysis done")
+
+
+print("proced to Save")
 ###################################################################################
 
 
 #################### S A V E  ######################
 ##################################################
 
+rm(list=to_clean,to_clean) #cleaning
 
-save.image("home/hachepunto/MARINa/Vaquerizas_MARINa.RData")
-savehistory("home/hachepunto/MARINa/Vaquerizas_MARINa.Rhistory")
+save.image("Vaquerizas_MARINa.RData")
+savehistory("Vaquerizas_MARINa.Rhistory")
+
+print("All done")
